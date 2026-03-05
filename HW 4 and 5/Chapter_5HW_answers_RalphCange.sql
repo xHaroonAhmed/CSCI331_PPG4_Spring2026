9a3844dfd0bfb3276d8e76c3a19bd2cab1ad0fd4
rcange-14 <ralphcange2@gmail.com>
2026-03-04 21:02:23 -0500

    Add files via upload

---
+++ HW 4 and 5/Chapter_5HW_answers_RalphCange.sql
@@ -0,0 +1,23 @@
+/*USE Northwinds2024Student
+SELECT OrderId AS orderid, OrderDate AS orderdate, CustomerId AS custid, EmployeeId AS empid,
+    DATEFROMPARTS(YEAR(OrderDate), 12, 31) AS endofyear
+FROM [Sales].[Order]
+WHERE OrderDate <> DATEFROMPARTS(YEAR(OrderDate), 12, 31);
+GO
+
+SINCE where happens before select the endofyear row didnt exist which can be fixed by declaring it the select clause*/
+
+/*USE Northwinds2024Student
+SELECT EmployeeId AS empid,MAX(OrderDate) AS maxorderdate
+FROM [Sales].[Order]
+GROUP BY EmployeeId
+ORDER BY EmployeeId;*/
+
+/*USE Northwinds2024Student
+
+
+SELECT TOP (10) OrderId AS orderid, OrderDate AS orderdate, CustomerId AS custid, EmployeeId AS empid,
+    ROW_NUMBER() OVER (ORDER BY OrderDate, OrderId) AS rownum
+FROM [Sales].[Order]
+ORDER BY rownum;*/
+
